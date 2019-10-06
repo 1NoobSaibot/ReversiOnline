8f7500803cad45c0355d0a4f9af9b6c42dc4ad90
Zheka Diulin <zheka.diulin@gmail.com>
2019-10-06 15:06:15 +0300

    Black/White counters in game-view

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <div>Это мой первый компонент Game</div>
+    <div>White({{wCounter}}) Black({{bCounter}}) </div>
     <Board :m="value ? value.board.m : null"/>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['value']
+  props: ['value'],
+  data: () => {
+    return {
+      wCounter: 0,
+      bCounter: 0
+    }
+  },
+  watch: {
+    value (game) {
+      this.wCounter = 0
+      this.bCounter = 0
+      if (!game) return
+      let m = game.board.m
+      for (let i = 0; i < m.length; i++) {
+        for (let j = 0; j < m[i].length; j++) {
+          if (m[i][j] === 1) this.wCounter++
+          else if (m[i][j] === 2) this.bCounter++
+        }
+      }
+    }
+  }
 }
 </script>
